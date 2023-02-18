3401ea2fb0df04aa7ce85b821cdd579675452740
heycristhian <heycristhian@gmail.com>
2023-02-18 19:36:11 -0300

    First commit

---
+++ src/test/java/br/com/senior/erp/ErpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senior.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
